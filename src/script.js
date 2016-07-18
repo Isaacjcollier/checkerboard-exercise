354bd05511c95f0fa9e2223248782f21c86008e0
Isaacjcollier <isaacjcollier@gmail.com>
2016-07-18 13:26:50 -0600

    complete checkerboard exercise

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// Your JS goes here
+for (var j = 0; j < 72 ; j+= 2) {
+  var background = document.createElement('div');
+  var redSquare = document.body.appendChild(background);
+  redSquare.style = "width: 11.1%; height: 100px; background-color: red; float: left "
+
+  var front = document.createElement('div');
+  var blkSquare = document.body.appendChild(front);
+  blkSquare.style = "width: 11.1%; height: 100px; background-color: black; float: left "
+}
